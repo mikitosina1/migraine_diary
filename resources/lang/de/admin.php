eab13f812c9080c5ed060d9335a77bad55125381
mikitosina1 <mikitosina27@gmail.com>
2025-08-14 00:10:23 +0200

    feat: added possible locales, added translations

---
+++ resources/lang/de/admin.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	'config_diary' => 'Konfigurieren',
+	'symptom_list' => 'Symptomliste',
+	'trigger_list' => 'Triggerliste',
+	'meds_list'    => 'Medikamentenliste',
+	'search'       => 'Suche',
+	'add'          => 'Hinzufügen',
+	'edit_item'    => 'Bearbeiten',
+	'code'         => 'Eindeutiger Code',
+	'code_tooltip' => 'auf Englisch, z. B.: Migräne oder Ibuprofen oder Übelkeit',
+	'name'         => 'Titel',
+	'save'         => 'Speichern',
+	'update'       => 'Aktualisierung',
+	'cancel'       => 'Abbrechen',
+];
---
+++ resources/lang/en/admin.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+	'config_diary' => 'Configure',
+	'symptom_list' => 'Symptoms list',
+	'trigger_list' => 'Triggers list',
+	'meds_list'    => 'Meds list',
+	'search'       => 'Search',
+	'add'          => 'Add',
+	'edit_item'    => 'Edit',
+	'code'         => 'Unique code',
+	'code_tooltip' => 'on english, ex: migraine or ibuprofen or nausea',
+	'name'         => 'Name',
+	'save'         => 'Save',
+	'update'       => 'Update',
+	'cancel'       => 'Cancel',
+];
