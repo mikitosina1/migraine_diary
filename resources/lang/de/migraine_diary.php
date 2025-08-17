50aeb6dda18896460fa4961121ee249e63cec846
mikitosina1 <mikitosina27@gmail.com>
2025-08-17 22:31:15 +0200

    feat: added translations for next steps

--- resources/lang/de/migraine_diary.php
+++ resources/lang/de/migraine_diary.php
@@ -3,4 +3,13 @@
 return [
 	'widget_title' => 'Migräne-Tagebuch',
 	'to_diary' => 'Nach Migräne-Tagebuch',
+	'add_attack' => 'Angriff hinzufügen',
+	'monthly_stats' => 'Monatsstatistik',
+	'triggers_stats' => 'Triggerstatistik',
+	'details' => 'Grundlegende Daten',
+	'symptoms' => 'Symptome',
+	'meds' => 'Medikamente',
+	'review' => 'Bestätigung',
+	'back' => 'Zurück',
+	'next' => 'Weiter',
 ];
--- resources/lang/en/migraine_diary.php
+++ resources/lang/en/migraine_diary.php
@@ -3,4 +3,13 @@
 return [
 	'widget_title' => 'Migraine Diary',
 	'to_diary' => 'To Migraine Diary',
+	'add_attack' => 'Add Attack',
+	'monthly_stats' => 'Monthly Stats',
+	'triggers_stats' => 'Triggers Stats',
+	'details' => 'Basic information',
+	'symptoms' => 'Symptoms',
+	'meds' => 'Meds',
+	'review' => 'Review',
+	'back' => 'Back',
+	'next' => 'Next',
 ];
