6dea38d1cda9d7c0bac16d5d76d0a1eb6d69b491
mikitosina1 <mikitosina27@gmail.com>
2025-09-28 01:47:11 +0200

    refactor: added return of function

--- App/Http/Controllers/MigraineDiaryController.php
+++ App/Http/Controllers/MigraineDiaryController.php
@@ -3,6 +3,9 @@
 namespace Modules\MigraineDiary\App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Modules\MigraineDiary\App\Http\Requests\AttackFilterRequest;
@@ -15,10 +18,14 @@ class MigraineDiaryController extends Controller
 		private readonly AttackFilterService $filterService,
 		private readonly AttackListRepository $listRepository
 	) {}
+
 	/**
 	 * Display a listing of the resource.
+	 *
+	 * @param AttackFilterRequest $request
+	 * @return Application|Factory|View
 	 */
-	public function index(AttackFilterRequest $request)
+	public function index(AttackFilterRequest $request): Application|Factory|View
 	{
 		$currentRange = $request->getRange();
 		$painLevel = $request->getPainLevel();
